61a07fc3a4953fbbf9ac868f8d747620f702986e
wildman082 <wildman@gmail.com>
2020-08-23 13:09:28 -0700

    don't create a snapshot if one is in progress

--- shotty.py
+++ shotty.py
@@ -17,6 +17,9 @@ def filter_instances(project):
     
     return instances
 
+def has_pending_snapshot(volume):
+    snapshots = list(volume.snapshots.all())
+    return snapshots and snapshots[0].state == 'pending'
 
 @click.group()
 def cli():
@@ -149,6 +152,8 @@ def create_snapshots(project):
         i.stop()
         i.wait_until_stopped()
         for v in i.volumes.all():
+            if has_pending_snapshot(v):
+                print("Skipping "+ v.id + ", snapshot already in progress")
             print("Creating snapshot of " + v.id + "...")
             v.create_snapshot(Description='Created by shotty',TagSpecifications=tag)
         print("Starting instance " + i.id + "...")
